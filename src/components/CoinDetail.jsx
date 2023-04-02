7e4e50f30f0f5389db480a28e0b17fc83fa71763
Truc Duong <trucdg@bu.edu>
2023-04-02 17:15:32 -0400

    use useEffect to fetch for data about each coin using its symbol

--- src/components/CoinDetail.jsx
+++ src/components/CoinDetail.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { Component, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function CoinDetail() {
+  let params = useParams();
+
+  const [fullDetails, setFullDetails] = useState(null);
+
+  useEffect(() => {
+    const getCoinDetail = async () => {
+      const details = await fetch(
+        `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${params.symbol}&tsyms=USD&api_key=` +
+          API_KEY
+      );
+
+      const description = await fetch(
+        `https://min-api.cryptocompare.com/data/all/coinlist?fsym=${params.symbol}&api_key=` +
+          API_KEY
+      );
+
+      const detailsJson = await details.json();
+      const descripJson = await description.json();
+
+      setFullDetails({
+        numbers: detailsJson.DISPLAY,
+        textData: descripJson.Data,
+      });
+    };
+
+    getCoinDetail().catch(console.error);
+  }, []);
+
   return (
     <div>
       <p>Coin Detail page</p>
